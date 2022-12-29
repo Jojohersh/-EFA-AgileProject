b796b2e09637d4ac8a89e1a454170b214538a6df
jordan <jojohersh@gmail.com>
2022-12-28 22:20:11 -0500

    created entity files and DbSets

--- Agile.Data/ApplicationDbContext.cs
+++ Agile.Data/ApplicationDbContext.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Agile.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Agile.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        
+        public DbSet<Box> Boxes {get; set;}
+        public DbSet<User> Users { get; set; }
+        public DbSet<Mail> Mail {get; set;}
     }
 }
