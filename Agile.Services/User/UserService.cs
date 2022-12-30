cd19631f8039fc3700e3973798e0c14f1f43b9a7
jordan <jojohersh@gmail.com>
2022-12-29 21:10:20 -0500

    added db context

--- Agile.Services/User/UserService.cs
+++ Agile.Services/User/UserService.cs
@@ -2,26 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Agile.Data;
 
 namespace Agile.Services.User
 {
     public class UserService : IUserService
     {
+        private readonly ApplicationDbContext _dbContext;
+        public UserService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         // Create
         public async Task<bool> CreateUserAsync() {
 
-        };
+        }
         // Read
         public async Task<UserEntity> GetUserAsync(int userId) {
 
-        };
+        }
         // Update
         Task<bool> UpdateUserAsync(int userId/*, make an update model*/) {
 
-        };
+        }
         // Delete
         Task<bool> DeleteUserAsync(int userId) {
             
-        };
+        }
     }
 }
