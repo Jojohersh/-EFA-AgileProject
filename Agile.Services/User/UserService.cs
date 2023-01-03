36a280f96e221f725b7b2ebab893de1cbece073f
jordan <jojohersh@gmail.com>
2023-01-02 20:38:46 -0500

    weird reference bug

--- Agile.Services/User/UserService.cs
+++ Agile.Services/User/UserService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Agile.Data;
+using Agile.Data.Entities;
 using Agile.Models.User;
 
 namespace Agile.Services.User
