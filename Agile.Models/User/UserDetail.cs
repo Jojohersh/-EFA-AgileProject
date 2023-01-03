f05eb75ec01408c325fc65780dd4c6fae2b3648c
jordan <jojohersh@gmail.com>
2023-01-02 20:56:41 -0500

    formatting

--- Agile.Models/User/UserDetail.cs
+++ Agile.Models/User/UserDetail.cs
@@ -8,7 +8,6 @@ namespace Agile.Models.User
     public class UserDetail
     {
         public int Id {get; set;}
-
         public string UserName {get; set;}
         public string EmailAddress {get; set;}
         
