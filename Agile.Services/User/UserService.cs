7e48db7f9b597e55d593b6968668583051ce8ffd
jordan <jojohersh@gmail.com>
2023-01-02 20:16:25 -0500

    added user register model added acceptance criteria to user service

--- Agile.Services/User/UserService.cs
+++ Agile.Services/User/UserService.cs
@@ -15,10 +15,19 @@ namespace Agile.Services.User
         }
 
         // Create
+        /*
+            Returns true if a valid email address and user's name are provided
+            returns false if either are missing or if email is not a valid email format
+            returns false if the provided model is invalid
+        */
         public async Task<bool> CreateUserAsync() {
 
         }
         // Read
+        /*
+            Returns a populated UserEntity from the database if the provided userId is valid
+            Returns a null object if the given userId is not a valid user
+        */
         public async Task<UserEntity> GetUserAsync(int userId) {
 
         }
